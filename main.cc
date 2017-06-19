f8200d311e3ae803061838623e982c1b30ef8d7d
Andrei <preda.andrei174@gmail.com>
2017-06-20 00:32:16 +0300

    Add linux support (I hope)

--- main.cc
+++ main.cc
@@ -5,11 +5,29 @@
 #include <stdexcept>
 #include <string>
 
+#ifdef _WIN32
 #include "windows.h"
+
+#elif __linux__
+#include <cstdio>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <vector>
+
+#else
+#error "OS not supported!"
+#endif
 
 using namespace std;
 
+#ifdef _WIN32
 const string kExecutableName = "supercalifragilistic.exe";
+#elif __linux__
+const string kExecutableName = "supercalifragilistic";
+const int kTestTimeout = 3000;
+#endif
 
 // Tests if a string has a certain ending
 inline bool ends_with(const string &str, const string &ending)
@@ -20,6 +38,7 @@ inline bool ends_with(const string &str, const string &ending)
     return equal(ending.rbegin(), ending.rend(), str.rbegin());
 }
 
+#ifdef _WIN32
 // Runs a process and waits for it to stop before continuing
 void RunAndWait(const string &process, const string &parameters = "")
 {
@@ -37,7 +56,65 @@ void RunAndWait(const string &process, const string &parameters = "")
     ShellExecuteEx(&sh_exec_info);
     WaitForSingleObject(sh_exec_info.hProcess, INFINITE);
 }
+#elif __linux__
+// Transforms a string into an array of its tokens as C strings
+char** Tokenize(const string &str)
+{
+    stringstream ss;
+    ss << str;
 
+    vector<string> tokens;
+    string token;
+
+    while (ss >> token) {
+        tokens.push_back(token);
+    }
+
+    // The token array will end with a NULL pointer (for exec)
+    char **c_tokens = (char**)malloc(sizeof(char*) * (tokens.size() + 1));
+
+    for (size_t i = 0; i < tokens.size(); ++i) {
+        c_tokens[i] = (char*)malloc(sizeof(char) * (tokens[i].size() + 3));
+	strcpy(c_tokens[i], tokens[i].c_str());
+	c_tokens[i][tokens[i].size()] = '\0';
+    }
+
+    // Add the null pointer at the end of the tokens array (for exec)
+    c_tokens[tokens.size()] = NULL;
+    return c_tokens;
+}
+
+// Runs a process and waits for it to stop before continuing
+void RunAndWait(const string &process, const string &parameters = "")
+{
+    string complete_command = process + " " + parameters;
+    pid_t my_pid;
+
+    if ((my_pid = fork()) == 0) {
+	execv(process.c_str(), Tokenize(complete_command));
+	perror("child process execv failed");
+	return;
+    } else if (my_pid > 0) {
+	wait(NULL); 
+    }
+}
+#endif
+
+#ifdef _WIN32
+// Changes the current working directory of the process
+inline void ChangeDirectory(const string &path)
+{
+    SetCurrentDirectory(path.c_str());
+}
+#elif __linux__
+// Changes the current working directory of the process
+inline void ChangeDirectory(const string &path)
+{
+    chdir(path.c_str());
+}
+#endif
+
+#ifdef _WIN32
 // Compiles the source whose name is given as a parameter
 // Decides automatically which compile instruction to use based on file extension
 void CompileSource(const string &source_name)
@@ -58,6 +135,28 @@ void CompileSource(const string &source_name)
     // Do the actual compiling
     RunAndWait(compile_process, compile_parameters);
 }
+#elif __linux__
+// Compiles the source whose name is gives as a parameter
+// Decides automatically which compile instruction to use based on file extension
+void CompileSource(const string &source_name)
+{
+    string compile_process = "";
+    string compile_parameters = "";
+
+    if (ends_with(source_name, ".c")) {
+        compile_process = "/usr/bin/gcc";
+	compile_parameters = "-Wall -Werror " + source_name + " -o " + kExecutableName;
+    } else if (ends_with(source_name, ".cpp") || ends_with(source_name, ".cc")) {
+        compile_process = "/usr/bin/g++";
+	compile_parameters = "-Wall -Werror -std=c++11 " + source_name + " -o " + kExecutableName;
+    } else {
+        throw invalid_argument(source_name + " does not have a supported file extension");
+    }
+
+    // Do the actual compiling
+    RunAndWait(compile_process, compile_parameters);
+}
+#endif
 
 int to_int(const string &str)
 {
@@ -200,7 +299,7 @@ int main(int argc, char* argv[])
     }
 
     // Navigate to target directory
-    SetCurrentDirectory(argv[1]);
+    ChangeDirectory(argv[1]);
 
     // Try to compile the source
     try {
